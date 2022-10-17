a689342ca1c5862ca158fd3b54aa2708425eb58c
Ahmed-ali-abd-elsalam <85034642+Ahmed-ali-abd-elsalam@users.noreply.github.com>
2022-10-17 21:43:25 +0200

    writing sbox function

--- desNew.cpp
+++ desNew.cpp
@@ -1,5 +1,10 @@
 #include <bits/stdc++.h>
 #include <math.h>
+#ifdef __GNUC__
+# define __rdtsc __builtin_ia32_rdtsc
+#else
+# include<intrin.h>
+#endif
 using namespace std;
 
 typedef unsigned long long u64;
@@ -112,29 +117,96 @@ u64 permutaion(u64 rightSide)
     return result;
 }
 
-// u64 sbox(u64 rightSide);
+u64 sbox(u64 rightSide){
+    int s[8][4][16] = {
+            { 14, 4,  13, 1, 2,  15, 11, 8,  3,  10, 6,  12, 5,
+            9,  0,  7,  0, 15, 7,  4,  14, 2,  13, 1,  10, 6,
+            12, 11, 9,  5, 3,  8,  4,  1,  14, 8,  13, 6,  2,
+            11, 15, 12, 9, 7,  3,  10, 5,  0,  15, 12, 8,  2,
+            4,  9,  1,  7, 5,  11, 3,  14, 10, 0,  6,  13 },
+            { 15, 1,  8,  14, 6,  11, 3, 4,  9,  7,  2,  13, 12,
+            0,  5,  10, 3,  13, 4,  7, 15, 2,  8,  14, 12, 0,
+            1,  10, 6,  9,  11, 5,  0, 14, 7,  11, 10, 4,  13,
+            1,  5,  8,  12, 6,  9,  3, 2,  15, 13, 8,  10, 1,
+            3,  15, 4,  2,  11, 6,  7, 12, 0,  5,  14, 9 },
+    
+            { 10, 0,  9,  14, 6,  3,  15, 5,  1,  13, 12,
+            7,  11, 4,  2,  8,  13, 7,  0,  9,  3,  4,
+            6,  10, 2,  8,  5,  14, 12, 11, 15, 1,  13,
+            6,  4,  9,  8,  15, 3,  0,  11, 1,  2,  12,
+            5,  10, 14, 7,  1,  10, 13, 0,  6,  9,  8,
+            7,  4,  15, 14, 3,  11, 5,  2,  12 },
+            
+            { 7,  13, 14, 3,  0,  6,  9,  10, 1,  2, 8,  5,  11,
+            12, 4,  15, 13, 8,  11, 5,  6,  15, 0, 3,  4,  7,
+            2,  12, 1,  10, 14, 9,  10, 6,  9,  0, 12, 11, 7,
+            13, 15, 1,  3,  14, 5,  2,  8,  4,  3, 15, 0,  6,
+            10, 1,  13, 8,  9,  4,  5,  11, 12, 7, 2,  14 },
+            
+            { 2,  12, 4, 1,  7,  10, 11, 6, 8,  5,  3,  15, 13,
+            0,  14, 9, 14, 11, 2,  12, 4, 7,  13, 1,  5,  0,
+            15, 10, 3, 9,  8,  6,  4,  2, 1,  11, 10, 13, 7,
+            8,  15, 9, 12, 5,  6,  3,  0, 14, 11, 8,  12, 7,
+            1,  14, 2, 13, 6,  15, 0,  9, 10, 4,  5,  3 },
+            
+            { 12, 1,  10, 15, 9,  2,  6,  8,  0,  13, 3, 4, 14,
+            7,  5,  11, 10, 15, 4,  2,  7,  12, 9,  5, 6, 1,
+            13, 14, 0,  11, 3,  8,  9,  14, 15, 5,  2, 8, 12,
+            3,  7,  0,  4,  10, 1,  13, 11, 6,  4,  3, 2, 12,
+            9,  5,  15, 10, 11, 14, 1,  7,  6,  0,  8, 13 },
+            
+            { 4,  11, 2,  14, 15, 0,  8, 13, 3,  12, 9,  7,  5,
+            10, 6,  1,  13, 0,  11, 7, 4,  9,  1,  10, 14, 3,
+            5,  12, 2,  15, 8,  6,  1, 4,  11, 13, 12, 3,  7,
+            14, 10, 15, 6,  8,  0,  5, 9,  2,  6,  11, 13, 8,
+            1,  4,  10, 7,  9,  5,  0, 15, 14, 2,  3,  12 },
+            
+            { 13, 2,  8, 4,  6,  15, 11, 1,  10, 9, 3, 14, 5,
+            0,  12, 7, 1,  15, 13, 8,  10, 3,  7, 4, 12, 5,
+            6,  11, 0, 14, 9,  2,  7,  11, 4,  1, 9, 12, 14,
+            2,  0,  6, 10, 13, 15, 3,  5,  8,  2, 1, 14, 7,
+            4,  10, 8, 13, 15, 12, 9,  0,  3,  5, 6, 11 }
+        };
+    u64 result=0;
+    cout<<s[7][1][1]<<endl;
+    for(int i=0;i<8;i++){
+        // get sbox
+        // cout<<result<<endl;
+        u64 idx = (rightSide>>(7-i)*6)&0x3f;
+        // reorder bits
+        idx = ((idx>>1)&15)|((idx&1)<<4)|(idx&0x20);
+        result |= (s[i][idx>>4][idx&0xf])<<((7-i)*4);
+        // cout<<result<<endl;
+    }
+    return result;
+}
 u64 leftshift(u64 keyhalf, int index)
 {
     int leftShiftTable[16] = {1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1};
     // store bit        shift left      set rightmost bits      clear leftmost bits
     int stored = keyhalf >> (28 - leftShiftTable[index]);
     keyhalf = (keyhalf << leftShiftTable[index]) | stored;
-    keyhalf = (keyhalf << (64 - 28)) >> (64 - 28);
+    keyhalf = keyhalf&0xffffff;
+    // keyhalf = (keyhalf << (64 - 28)) >> (64 - 28);
     return keyhalf;
 }
-u64 round(u64 plaintext, u64 key)
+u64 singleRound(u64 plaintext, u64 key)
 {
 
     // intitaal permutation and plain text split
     plaintext = intialPermutation(plaintext);
     u64 leftText = plaintext >> 32;
-    u64 rightText = (plaintext << 32) >> 32;
+    // hex for 32 bits
+    u64 rightText = plaintext&0xffffffff;
+    // u64 rightText = (plaintext << 32) >> 32;
     u64 cypherText = 0;
 
     // key permutation and split
     key = permutationChoiceKey1(key);
     u64 keyLeft = (key >> 28);
-    u64 keyRight = (key << (64 - 28)) >> (64 - 28);
+    // hex for 28 bits
+    u64 keyRight = key&0xffffff;
+    // u64 keyRight = (key << (64 - 28)) >> (64 - 28);
 
     for (int i = 0; i < 16; i++)
     {
@@ -160,5 +232,17 @@ u64 round(u64 plaintext, u64 key)
 }
 
 int main(){
+    long long t1=__rdtsc();
+    // code here
+    u64 x = 3;
+    sbox(x);
+    u64 lastDigit = sbox(x)&0x3f;
+    cout<<lastDigit;
+
+
+
+    long long t2=__rdtsc();
+    // printf("Cycles: %lld\n", t2-t1);
+
 
 }
