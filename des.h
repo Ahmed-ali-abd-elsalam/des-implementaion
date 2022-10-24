52c0c1897c967ca0814dab22e91f38ce846dad8a
LotfyMabrouk <85258381+LotfyMabrouk@users.noreply.github.com>
2022-10-24 14:05:27 +0200

    Update des.h

--- des.h
+++ des.h
@@ -1,17 +1,23 @@
-#ifndef DES_H
-#define DES_H
-#include<bits/stdc++.h>
-#include<fstream>
-using namespace std;
-string BinToHex(string s);
-string HexToBin(char s);
-bitset<64> intialPermutation(bitset<64>plainText);
-bitset<64> inverseIntialPermutation(bitset<64>plainText);
-bitset<64> permutationChoiceKey1(bitset<64> key);
-bitset<64> permutationChoiceKey2(bitset<64> key);
-bitset<64> expansion(bitset<64> rightSide);
-// bitset<64> sbox(bitset<64> rightSide);
-// bitset<64> leftshift(bitset<64> keyhalf);
-bitset<64> permutaion(bitset<64> rightSide);
-void round(bitset<64> plaintext,bitset<64>key);
+#include <bits/stdc++.h>
+#ifdef __GNUC__
+#define __rdtsc __builtin_ia32_rdtsc
+#else
+#include <intrin.h>
 #endif
+using namespace std;
+typedef unsigned long long u64;
+u64 readDESInputhex(const char *data);
+u64 messagePlainHelper(int x, u64 d, int i);
+void readMessagePlain(char mode);
+u64 intialPermutation(u64 plainText);
+u64 inverseIntialPermutation(u64 plainText);
+u64 permutationChoiceKey1(u64 key);
+u64 permutationChoiceKey2(u64 key);
+u64 expansion(u64 rightSide);
+u64 permutaion(u64 rightSide);
+u64 sbox(u64 rightSide);
+u64 leftshift(u64 keyhalf, int index);
+u64 feistelFunction(u64 rightside, u64 key);
+u64 singleRound(u64 plaintext, u64 key, char mode);
+void outputPlainText(u64 cypher, char mode);
+
